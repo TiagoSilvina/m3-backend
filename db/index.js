07a61d5bfdff4ff121d5bf0a30429d92169258ba
TiagoSilvina <tiagoafsilvina@gmail.com>
2024-03-14 12:29:32 +0000

    deploy database

--- db/index.js
+++ db/index.js
@@ -5,8 +5,7 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/3-module-project";
+const MONGO_URI = "mongodb+srv://tiagoafsilvina:1ronHack.@m3-project.r8yh2t0.mongodb.net/?retryWrites=true&w=majority&appName=M3-project"
 
 mongoose
   .connect(MONGO_URI)
